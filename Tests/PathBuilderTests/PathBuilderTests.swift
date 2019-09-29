9715e209e03e37d0c009593ab7f24a7e45ce8d95
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-09-29 21:15:34 +0200

    Add basic component tests

--- Tests/PathBuilderTests/PathBuilderTests.swift
+++ Tests/PathBuilderTests/PathBuilderTests.swift
@@ -3,15 +3,44 @@ import PathBuilder
 import SwiftUI
 
 final class PathBuilderTests: XCTestCase {
-    func testArray() {
+
+    func testAllComponents() {
         let path = Path {
-            Move(to: CGPoint(x: 100, y: 100))
-            Line(CGPoint(x: 200, y: 200))
+            Arc(center: .zero, radius: .zero, startAngle: .zero, endAngle: .zero, clockwise: true)
+            Close()
+            Ellipse(in: .zero)
+            Move(to: .zero)
+            Line(.zero)
+            QuadCurve(to: .zero, control: .zero)
+            Rect(.zero)
+            RelativeArc(center: .zero, radius: .zero, startAngle: .zero, delta: .zero)
+            RoundedRect(in: .zero, cornerSize: .zero)
+            Subpath(path: CGMutablePath())
+            TangentArc(end1: .zero, end2: .zero, radius: .zero)
         }
-        XCTAssertEqual(CGRect(x: 100, y: 100, width: 100, height: 100), path.boundingRect)
+        XCTAssertEqual(path.boundingRect, .zero)
+    }
+
+    func testSubpaths() {
+        let path = Path {
+            Subpath {
+                Move(to: CGPoint(x: 100, y: 100))
+                Line(.zero)
+            }
+            Subpath {
+                Move(to: CGPoint(x: 100, y: 0))
+                Line(CGPoint(x: 0, y: 100))
+            }
+            Subpath {
+                Move(to: CGPoint(x: 0, y: 100))
+                Line(CGPoint(x: 100, y: 0))
+            }
+        }
+        XCTAssertEqual(CGRect(origin: .zero, size: CGSize(width: 100, height: 100)), path.boundingRect)
     }
 
     static var allTests = [
-        ("testExample", testArray),
+        ("testAllComponents", testAllComponents),
+        ("testSubpaths", testSubpaths),
     ]
 }
