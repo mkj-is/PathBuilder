1fe7a41ad5c851714609f25ab246f1c192891075
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2021-02-21 11:19:43 +0100

    Add build methods documentation

--- Sources/PathBuilder/PathBuilder.swift
+++ Sources/PathBuilder/PathBuilder.swift
@@ -3,10 +3,15 @@ import SwiftUI
 /// A custom result builder which constructs Paths from closures.
 @resultBuilder
 public struct PathBuilder {
+    
+    /// Enables support for 'for..in' loops by combining the
+    /// results of all iterations into a single result.
     public static func buildArray(_ components: [PathComponent]) -> PathComponent {
         components
     }
 
+    /// Required by every result builder to build combined results from
+    /// statement blocks.
     public static func buildBlock(_ components: PathComponent...) -> PathComponent {
         components
     }
@@ -16,19 +21,27 @@ public struct PathBuilder {
         component ?? EmptySubpath()
     }
 
+    /// With buildEither(second:), enables support for 'if-else' and 'switch'
+    /// statements by folding conditional results into a single result.
     public static func buildEither(first: PathComponent) -> PathComponent {
         first
     }
 
+    /// With buildEither(first:), enables support for 'if-else' and 'switch'
+    /// statements by folding conditional results into a single result.
     public static func buildEither(second: PathComponent) -> PathComponent {
         second
     }
     
+    /// This will be called on the partial result of an 'if
+    /// #available' block to allow the result builder to erase type
     /// information.
     public static func buildLimitedAvailability(_ component: PathComponent) -> PathComponent {
         component
     }
     
+    /// This will be called on the partial result from the outermost
+    /// block statement to produce the final returned result Path.
     public static func buildFinalResult(_ component: PathComponent) -> Path {
         Path { path in
             component.add(to: &path)
